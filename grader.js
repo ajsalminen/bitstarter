cc5c77aea8141142b12cd3225980871d44be601a
Antti Salminen <antti.salminen@gmail.com>
2013-07-11 01:10:48 +0300

    Implement url support for grader and add classes to checks.json.

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -44,8 +45,7 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function($, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,10 +65,28 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+  var checkHtmlUrl = function(html) {
+    if (html instanceof Error) {
+      console.error('Error: ' + util.format(response.message));
+    } else {
+      var checkJson = checkHtmlFile(cheerio.load(html), program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
+
+  }
+
+  if(program.url){
+    rest.get(program.url).on('complete', checkHtmlUrl);
+  } else if(program.file){
+    html = cheerioHtmlFile(program.file);
+    var checkJson = checkHtmlFile(html, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
-} else {
-    exports.checkHtmlFile = checkHtmlFile;
-}
+  }
+    } else {
+      exports.checkHtmlFile = checkHtmlFile;
+    }
